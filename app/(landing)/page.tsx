a6b30ee8dc486a17fd20e9d480987e50b9205b5d
kevin <kevingitabr@gmail.com>
2024-08-07 06:37:25 +0530

    completed landing page hero section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,20 @@
 import { Button } from '@/components/ui/button';
+import Heading from './_components/heading';
+import { Heroes } from './_components/heroes';
+import Image from 'next/image';
 
-export default function Home() {
+
+
+const LandingPage = () => {
   return (
-    <div>
-      <Button>
-        Click me
-      </Button>
+    <div className='min-h-full flex flex-col'>
+      <div className='flex flex-col items-center justify-center md:justify-start text-center gapy-y-8 flex-1 px-6 pb-10'>
+        <Heading />
+        <Heroes />
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
+
+export default LandingPage;
