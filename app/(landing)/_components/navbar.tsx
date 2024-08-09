a86e3817ab5dc17e9533140594eb864a98a43a90
kevin <kevingitabr@gmail.com>
2024-08-09 14:43:36 +0530

    integrated convex an clerk

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Spinner } from "@/components/spinner";
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
+  const { isLoading, isAuthenticated } = useConvexAuth();
+
   return (
     <nav className="flex w-full items-center bg-background p-6">
       <Image
@@ -13,9 +20,22 @@ const Navbar = () => {
         className="hidden md:block"
       />
 
-      <div className="flex w-full md:ml-auto md:justify-end">
-        {/* Login */}
-        <ModeToggle/>
+      <div className="flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
+        {isLoading && <Spinner/>}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"} size={"sm"}>
+                Login
+              </Button>
+            </SignInButton>
+
+            <SignInButton mode="modal">
+              <Button size={"sm"}>Get Notion free</Button>
+            </SignInButton>
+          </>
+        )}
+        <ModeToggle />
       </div>
     </nav>
   );
